76571e68b1379063650e51d017e5c2bae0c3cac9
Murilo Maia <murilomaiab@gmail.com>
2023-05-12 14:33:22 -0300

    chore: config vitest

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tsConfigTest from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsConfigTest()],
+  test: {
+    globals: true
+  }
+})
