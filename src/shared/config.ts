df650b76f380e1d67ee1892434ecd7a79e990c5a
Murilo Maia <murilomaiab@gmail.com>
2023-05-14 22:00:31 -0300

    refactor: import dotenv/config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,4 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 export default {
   openAIApiKey: process.env.OPEN_AI_API_KEY,
